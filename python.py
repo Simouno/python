286718a913cf131da5f4ddd627d327da50432e4c
Simouno <58078127+Simouno@users.noreply.github.com>
2020-01-17 16:11:49 -0800

    Create python.py

---
+++ python.py
@@ -0,0 +1,10 @@
+reponce=str(0)
+reponce1=str(oui)
+reponce2=str(non)
+
+print('Aime tu la couleur rouge?')
+reponce=str(input('reponce='))
+if reponce == reponce1:
+  print('Cest génial!')
+elseif reponce == reponce2:
+  print('Ah, dacord')
